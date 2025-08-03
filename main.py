d8198540caf42f426cbc7e0c5eaa352ef088cf30
na1353 <nasirvh13531@gmail.com>
2025-08-03 16:52:46 +0330

    main.py

--- main.py
+++ main.py
@@ -1,24 +1,17 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
 
-BOT_TOKEN = "8211367936:AAHS--KZH3uGDe6Wke6egtZ9cEiI1hRUgg4"
+BOT_TOKEN = '8211367936:AAHS--KZH3uGDe6Wke6egtZ9cEiI1hRUgg4'
 
 
-# وقتی کاربر /start می‌فرسته
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("✅ ربات آماده است.")
+    await update.message.reply_text("✅ ربات آماده است. یک پیام بفرست.")
 
-# وقتی کاربر متن معمولی می‌فرسته
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if update.message.text:
-        await update.message.reply_text(f"📨 متن شما: {update.message.text}")
+    await update.message.reply_text(f"📨 پیام دریافت شد:\n{update.message.text}")
 
-# ساخت ربات با توکن
-app = ApplicationBuilder().token(BOT_TOKEN).build()
-
-# تعریف دستورات
-app.add_handler(CommandHandler("start", start))
-app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
-
-# اجرای ربات
-app.run_polling()
+if __name__ == "__main__":
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+    app.run_polling()
